60b29ad65fb5ac8ea558e5176e7073ab859f225d
alysson <alysson.cirilo@gmail.com>
2019-05-05 14:39:46 -0300

    start coding

---
+++ code/semanticserver/src/main/java/br/ufma/lsdi/semanticserver/SemanticserverApplication.java
@@ -0,0 +1,13 @@
+package br.ufma.lsdi.semanticserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticserverApplication.class, args);
+	}
+
+}
---
+++ code/semanticserver/src/test/java/br/ufma/lsdi/semanticserver/SemanticserverApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufma.lsdi.semanticserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemanticserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
